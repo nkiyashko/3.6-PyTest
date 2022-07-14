59b7a9804488cecc45b717d049d9e976bd3eb731
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2022-07-14 11:41:23 +0300

    search via es lang was added

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: es or en")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    browser = webdriver.Chrome()
+    browser.get(f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+def button_is_present(browser):
+    try:
+        browser.implicitly_wait(10)
+        browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+        return True
+    except:
+        return False
+
+
+def test_feedback(browser):
+    assert button_is_present(browser) == True, "Button is not found"
